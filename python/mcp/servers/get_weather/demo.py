d96e5eddf2518cf66c0dc2ab0c7c2c08cef2606b
Ryu0n <hsung951027@gmail.com>
2026-03-08 21:32:54 +0900

    ✨ c: Demonstrate MCP Server and Client

---
+++ python/mcp/servers/get_weather/demo.py
@@ -0,0 +1,21 @@
+import random
+import asyncio
+from fastmcp import FastMCP
+
+mcp = FastMCP("Demo 🚀")
+
+@mcp.tool
+def add(a: int, b: int) -> int:
+    """Add two numbers"""
+    return a + b
+
+@mcp.tool
+def roll_dice(n_dice: int) -> list[int]:
+    """Roll `n_dice` 6-sided dice and return the results."""
+    return [random.randint(1, 6) for _ in range(n_dice)]
+
+if __name__ == "__main__":
+    # mcp.run()
+    asyncio.run(
+        mcp.run_http_async()
+    )
